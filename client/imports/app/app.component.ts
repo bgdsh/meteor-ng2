93a0fb2a3a3c9438cbd98f525a9f9e6e6558fabf
bgd_sh <bgd_sh@hotmail.com>
2016-12-03 21:57:53 +0800

    finished lesson: 3-way Data Binding 🏥

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,27 +1,16 @@
 import { Component } from '@angular/core';
- 
+import { Parties } from '../../../both/collections/parties.collection'
+import { Party } from '../../../both/models/party.collection'
 import template from './app.component.html';
- 
+import { Observable } from 'rxjs/Observable'
+
 @Component({
   selector: 'app',
   template
 })
 export class AppComponent {
-  private parties : any[]
+  private parties: Observable<Party[]>
   constructor() {
-    this.parties = [
-      {'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.',
-        'location': 'Palo Alto'
-      },
-      {'name': 'All dubstep all the time',
-        'description': 'Get it on!',
-        'location': 'Palo Alto'
-      },
-      {'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.',
-        'location': 'San Francisco'
-      }
-    ];
+    this.parties = Parties.find({}).zone();
   }
 }
